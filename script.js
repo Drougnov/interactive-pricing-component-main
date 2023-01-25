741ae10075f37980d02537c955f6996f3c28d2b8
RockyBaura <251drougnov@gmail.com>
2023-01-25 23:10:11 +0600

    Refactor: [removed D.R.Y code]

--- script.js
+++ script.js
@@ -8,35 +8,30 @@ const value = [0, 20, 40, 60, 80];
 const cost = [08, 12, 16, 24, 32]
 const views = ['10K', '50K', '100K', '500K', '1M'];
 
-toggleYearly.addEventListener('change', (e)=>{
-    for(let i=0; i< value.length; i++){
-        if(rangeInput.value >= value[i]){
-            pageViews.innerText = views[i];
-            if(toggleYearly.checked){
-                price.innerText = `$${cost[i] * .75}.00`;
-                toggleText.innerText = "/ year";
-            }else{
-                price.innerText = `$${cost[i]}.00`;
-                toggleText.innerText = "/ month";
-            }
-        }
-    }
-})
+toggleYearly.addEventListener('change', handleToggleYearly);
+rangeInput.addEventListener('input', handleRangeInput);
 
-rangeInput.addEventListener('input', (e)=>{
-    let inputValue = e.target.value;
-    plan(inputValue)
-})
+function handleToggleYearly(e) {
+    plan(rangeInput.value);
+}
 
-function plan(inputValue){
-    for(let i=0; i< value.length; i++){
-        if(inputValue >= value[i]){
+function handleRangeInput(e) {
+    const inputValue = e.target.value;
+    rangeInput.style.background = `linear-gradient(
+                                        to right,
+                                        hsl(174, 77%, 80%) 0%,
+                                        hsl(174, 77%, 80%) ${(inputValue * 100) / 80}%,
+                                        hsl(224, 65%, 95%) 50%,
+                                        hsl(224, 65%, 95%) 100%)`;
+    plan(inputValue);
+}
+
+function plan(inputValue) {
+    for(let i = 0; i < value.length; i++) {
+        if (inputValue >= value[i]) {
             pageViews.innerText = views[i];
-            if(toggleYearly.checked){
-                price.innerText = `$${cost[i] * .75}.00`;
-            }else{
-                price.innerText = `$${cost[i]}.00`;
-            }
+            price.innerText = `$${(toggleYearly.checked ? cost[i] * .75 : cost[i]).toFixed(2)}`;
+            toggleText.innerText = toggleYearly.checked ? '/ year' : '/ month';
         }
     }
 }
